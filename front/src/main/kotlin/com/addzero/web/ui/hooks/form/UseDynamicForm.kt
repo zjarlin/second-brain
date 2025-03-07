9dd8a08e510ba8159eaf056d1696325ad363f17b
zjarlin <zjarlin@outlook.com>
2025-03-07 21:53:39 +0800

    change

--- front/src/main/kotlin/com/addzero/web/ui/hooks/form/UseDynamicForm.kt
+++ front/src/main/kotlin/com/addzero/web/ui/hooks/form/UseDynamicForm.kt
@@ -2,14 +2,114 @@ package com.addzero.web.ui.hooks.form
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.addzero.common.kt_util.toNotBlankStr
 import com.addzero.web.ui.hooks.UseHook
 import com.addzero.web.ui.hooks.table.common.UseTableContent
 import com.addzero.web.ui.hooks.table.entity.AddColumn
+import com.addzero.web.ui.hooks.table.entity.RenderType.*
+
+@Composable
+fun <E : Any> FormItem(columnMeta: AddColumn<E>, useDynamicForm: UseDynamicForm<E>?) {
+    val currentFormItem = useDynamicForm?.currentFormItem
+
+    val renderType = columnMeta.renderType
+    val getFun = columnMeta.getFun
+    val setFun = columnMeta.setFun
+
+    val fieldValue = currentFormItem?.let { getFun(it) }
+
+    val validRes = currentFormItem?.let { columnMeta.validator(it) }
+
+    val text = fieldValue.toNotBlankStr()
+    when (renderType) {
+        TEXT -> {
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newval ->
+                    useDynamicForm ?: return@OutlinedTextField
+                    useDynamicForm.currentFormItem = setFun(currentFormItem, columnMeta, newval)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text(columnMeta.placeholder) },
+                isError = validRes == false,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            )
+        }
+
+        IMAGE -> {
+            Text(text)
+        }
+
+        CUSTOM -> {
+        }
+
+        TEXTAREA -> {
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newval ->
+
+                    useDynamicForm ?: return@OutlinedTextField
+
+
+                    useDynamicForm.currentFormItem = setFun(currentFormItem, columnMeta, newval)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text(columnMeta.placeholder) },
+                isError = !columnMeta.validator(currentFormItem),
+//                singleLine = true,
+                maxLines = 5,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            )
+
+        }
+
+        SWITCH -> {
+            Switch(
+                checked = fieldValue == true,
+                onCheckedChange = {
+
+                    useDynamicForm ?: return@Switch
+                    useDynamicForm.currentFormItem = setFun(currentFormItem, columnMeta, it)
+                }
+            )
+        }
+
+        TAG -> {
+            Text(text)
+        }
+
+        NUMBER -> {}
+        LINK -> {}
+        DATE -> {}
+        DATETIME -> {}
+        SELECT -> {}
+        MULTISELECT -> {}
+        CHECKBOX -> {}
+        RADIO -> {}
+        CODE -> {}
+        HTML -> {}
+        MONEY -> {}
+        CURRENCY -> {}
+        PERCENT -> {}
+        BAR -> {}
+        TREE -> {}
+        COMPUTED -> {}
+        AUTO_COMPLETE -> {}
+        FILE -> {}
+    }
+
+}
 
 class UseDynamicForm<E : Any>(
     private val useTableContent: UseTableContent<E>,
@@ -34,6 +134,7 @@ class UseDynamicForm<E : Any>(
         val setFun = addColumn.setFun
         currentFormItem = setFun(currentFormItem, addColumn, newval)
     }
+
 
     override val render: @Composable () -> Unit
         get() = {
